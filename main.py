70efd623f1a0a6879c0c910651cc06e4a7197894
KDast <daoustkevin07@gmail.com>
2024-11-24 08:50:26 -0500

    B: letter count in progress

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
  book_path = "books/frankenstein.txt"
  text = get_book_text(book_path)
  num_words = get_num_words(text)
+ num_letters = get_num_char(text)
  print(f"{num_words} words found in the document")
+ print(f"{num_letters} letters found in the document")
+ 
  
 
 def get_num_words(text):
@@ -16,8 +19,18 @@ def get_book_text(path):
   return f.read()
 
 def get_num_char(text):
- words = text.split()
+ char_count = {}
+ char = text.lower()
+ words = char.split()
+ # Cannot split a list I need to iterate over each string
+ # letters = words.split()
   
+ return len(letters)
+ 
+
+
+ 
+ 
  
 
 main()
